8d67926572c63dc080aafb5b96004f1d3906e4f1
Ruqayat <yinkaajibade97@gmail.com>
2021-06-18 05:43:34 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,19 @@ import { Link } from "react-router-dom";
 // import { useContext } from "react";
 // import { AppContext } from "../components/stateprovider";
 
-import '../styles/home.css'
+import "../styles/home.css";
 
 function Home() {
-
   return (
     <div className="app">
       <h1 id="home-title">Welcome to My TodoList App</h1>
-      <Link to="/login">Click here to Login</Link>
+      <p>
+        <Link to="/login">Click here to Login</Link>
+          /
+        <Link to="/register">Register</Link>
+      </p>
     </div>
   );
-};
+}
 
 export default Home;
